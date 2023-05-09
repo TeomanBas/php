aae18fb8070ec7e08ad7f3595de6a98cadc30149
Teoman BAŞ <teomanbas@protonmail.com>
2023-05-08 22:42:21 -0400

    31_UstKapsamDegiskeni.php için ek açıklama eklendi.

--- 31_UstKapsamDegiskeni.php
+++ 31_UstKapsamDegiskeni.php
@@ -1,10 +1,20 @@
 <?php 
 $pi=3.14;
+$mesaj="hesapama tamamlandı";
+
+/* bir fonksiyon tanımlarken dışarıki değişkene doğrudan erişemeyiz 
+   dışarıdaki değişkene erişmek için fonksiyon içerisinde global yada $GLOBALS['değişken_adı']
+    şeklinde erişmemiz gereklidir
+ */
 function DaireAlanCevre($r){
     global $pi;
     $Alan =$pi*($r^2);
     $Cevre=2*$pi*$r;
     print("alan : ".$Alan." | "."cevre : ".$Cevre);
+    print("<br>");
+    echo $GLOBALS['mesaj'];
+    // aşağıdaki şeklildeki gibi bir kullanım hata verecektir.
+    echo $mesaj;
 }
 
 DaireAlanCevre(101);
